930974e455e578463283be71ebc6817257d62447
Yordi <yorditjordin@gmail.com>
2023-04-11 15:56:17 +0300

    Webpack initiator setup correctly
    - Webpack load html, style and javascript
    - all are in /src directoy
    - Happy time using is as template

--- src/webpack_run.js
+++
@@ -1,3 +0,0 @@
-import './style.css';
-import { camelCase } from 'lodash';
-console.log(camelCase('hello WEBPACK'))
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,37 @@
+/*eslint-disable*/
+
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/webpack_run.js',
-    output: {
-        filename: 'webpack_is_run.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    module: {
-        rules: [
-        {
-            test: /\.scss$/,
-            use: [
-            'style-loader',
-            'css-loader',
-            'scss-loader'
-            ], 
-        },
+  mode: 'development',
+  entry: {
+    index: './src/script.js', // Entry point for the application
+  },
+  // devtool: 'inline-source-map',
+  devServer: {
+    static: './dist', // Serve static files from the 'dist' directory
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html', // Use the index.html file as a template
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js', // Use the entry point name for the bundle filename
+    path: path.resolve(__dirname, 'dist'), // Output to the 'dist' directory
+    clean: true, // Clean the output directory before building
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'], // Process CSS files with 'style-loader' and 'css-loader'
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource', // Process image files as assets
+      },
     ],
-},    
+  },
 };
